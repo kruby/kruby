23c2f1560bb26fbd41495cf4739f17bfadee014a
Thomas K. Schmidt <ts@kruby.dk>
2010-01-02 03:31:35 +0100

    Flere posts på forsiden.

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -23,7 +23,7 @@ class ViewerController < ApplicationController
     @pagetitle = @page.title rescue 'Indhold følger snarest'
     @content = @page.body rescue 'Indhold følger snarest'
     @headline = @page.headline rescue 'Indhold følger snarest'
-    @posts = Post.forside_blogs_active.all(:limit => 3)
+    @posts = Post.forside_blogs_active.all(:limit => 10)
     
     # if test_web_browser == "Firefox3"
     #       render :action => "show_bad_browsers"
