27bb2f0a0f50123bad75e09dc5732f7aa72fe94a
Thomas K. Schmidt <ts@kruby.dk>
2010-12-21 07:58:02 +0100

    Vi prøver med google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,9 +15,21 @@ module ApplicationHelper
   #    end
   #  end
      
- 
-
+  def google_analytics_js
+    "<script type='text/javascript'>
   
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-20355035-1']);
+      _gaq.push(['_trackPageview']);
+  
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+  
+    </script>"
+  end
   
   def image
       if @image
