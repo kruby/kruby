915178895db423d2645fb4628cff68396fd1d63a
Thomas K. Schmidt <ts@kruby.dk>
2012-08-26 21:25:00 +0200

    Beregninger til købte og brugte timer pr. halvår

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -13,5 +13,11 @@ class Hour < ActiveRecord::Base
   
   scope :year_hours, lambda { |this_year| where("date BETWEEN '#{this_year.to_datetime.beginning_of_year}' AND '#{this_year.to_datetime.end_of_year}'") }
   
+  scope :last_year_first_6_months, lambda { |this_date| where("date BETWEEN '#{this_date.to_datetime.beginning_of_year}' AND '#{this_date.to_datetime.beginning_of_year+6.months-1.day}'") }
+  scope :last_year_last_6_months, lambda { |this_date| where("date BETWEEN '#{this_date.to_datetime.beginning_of_year+6.months}' AND '#{this_date.to_datetime.end_of_year}'") }
+  
+  scope :this_year_last_6_months, lambda { |this_date| where("date BETWEEN '#{this_date.to_datetime.beginning_of_year+6.months}' AND '#{this_date.to_datetime.end_of_year}'") }
+  scope :this_year_first_6_months, lambda { |this_date| where("date BETWEEN '#{this_date.to_datetime.beginning_of_year}' AND '#{this_date.to_datetime.beginning_of_year+6.months-1.day}'") }
+  
   #scope :customer_hours, lambda { |relation_id, this_month| where(:relation_id => relation_id) and where("date BETWEEN '#{this_month.to_datetime.beginning_of_month}' AND '#{this_month.to_datetime.end_of_month}'")  }
 end
--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,3 +1,21 @@
 class Voucher < ActiveRecord::Base
+  
   belongs_to :relation
+  
+  scope :last_year_first_6_months, lambda { |this_date| where("date BETWEEN '#{this_date.to_datetime.beginning_of_year}' AND '#{this_date.to_datetime.beginning_of_year+6.months-1.day}'") }
+  scope :last_year_last_6_months, lambda { |this_date| where("date BETWEEN '#{this_date.to_datetime.beginning_of_year+6.months}' AND '#{this_date.to_datetime.end_of_year}'") }
+  
+  scope :this_year_last_6_months, lambda { |this_date| where("date BETWEEN '#{this_date.to_datetime.beginning_of_year+6.months}' AND '#{this_date.to_datetime.end_of_year}'") }
+  scope :this_year_first_6_months, lambda { |this_date| where("date BETWEEN '#{this_date.to_datetime.beginning_of_year}' AND '#{this_date.to_datetime.beginning_of_year+6.months-1.day}'") }    
+  
+  #scope :this_half_year(which_half_of_year(this_date))
+  
+  # def self.which_half_of_year(this_date)
+  #     if this_date.to_date.month > 6
+  #        Voucher.these_last_6_months(this_date)
+  #       else
+  #        Voucher.these_first_6_months(this_date)
+  #     end
+  #   end
+  
 end
